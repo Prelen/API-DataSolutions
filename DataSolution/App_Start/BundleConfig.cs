a977b7f289b107325618e5744d8a7393bc1c2707
Prelen <prelen@gmail.com>
2019-02-07 23:46:53 +0200

    New home page and scripts

--- DataSolution/App_Start/BundleConfig.cs
+++ DataSolution/App_Start/BundleConfig.cs
@@ -25,6 +25,30 @@ namespace DataSolution
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/theme").Include(
+                "~/Assets/libs/popperjs/dist/umd/popper.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Assets/libs/perfect-scrollbar/dist/perfect-scrollbar.min.js",
+                "~/Assets/extra-libs/sparkline/sparkline.js",
+                "~/Assets/dist/js/waves.js",
+                "~/Assets/dist/js/sidebarmenu.js",
+                "~/Assets/dist/js/custom.min.js",
+                "~/Assets/libs/flot/excanvas.js",
+                "~/Assets/libs/flot/jquery.flot.js",
+                "~/Assets/libs/flot/jquery.flot.pie.js",
+                "~/Asset/libs/flot/jquery.flot.time.js",
+                "~/Asset/libs/flot/jquery.flot.stack.js",
+                "~/Asset/libs/flot/jquery.flot.crosshair.js",
+                "~/Asset/libs/flot.tooltip/js/jquery.flot.tooltip.min.js",
+                "~/Assets/dist/js/pages/chart/chart-page-init.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/themecss").Include(
+                    "~/Assets/dist/css/icons/font-awesome/css/fontawesome-all.css",
+                    "~/Assets/dist/css/icons/themify-icons/themify-icons.css",
+                    "~/Assets/dist/css/icons/material-design-iconic-font/css/materialdesignicons.min.css"
+                ));
         }
     }
 }
---
+++ DataSolution/Areas/External/Controllers/ExternalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataSolution.Areas.External.Controllers
+{
+    public class ExternalController : Controller
+    {
+        // GET: External/External
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            ViewBag.Title = "Home";
+
+            return View();
+        }
+    }
+}
